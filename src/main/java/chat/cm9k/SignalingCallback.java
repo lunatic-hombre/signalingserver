79201b51c83794760da88591600cc6cada63b287
peter.urbanus <peter.urbanus@servicenow.com>
2019-11-28 12:27:32 +0100

    Added command-handling: HELLO, OFFER, ANSWER and WHO

--- src/main/java/chat/cm9k/SignalingCallback.java
+++ src/main/java/chat/cm9k/SignalingCallback.java
@@ -1,11 +1,18 @@
 package chat.cm9k;
 
+import java.security.InvalidParameterException;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.jsoniter.output.JsonStream;
+
 import io.undertow.websockets.WebSocketConnectionCallback;
 import io.undertow.websockets.core.*;
 import io.undertow.websockets.spi.WebSocketHttpExchange;
 
 public class SignalingCallback implements WebSocketConnectionCallback {
 
+private static Set<String> clients = new HashSet<>();
 
     public void onConnect(WebSocketHttpExchange exchange, WebSocketChannel channel) {
         String path = exchange.getRequestURI();
@@ -17,19 +24,58 @@ public class SignalingCallback implements WebSocketConnectionCallback {
 
     private AbstractReceiveListener createChannel(WebSocketHttpExchange exchange) {
         return new AbstractReceiveListener() {
-            @Override
+            
+        	@Override
             protected void onFullTextMessage(WebSocketChannel channel, BufferedTextMessage message) {
                 String messageString = message.getData();
-                System.out.println("Message received: " + messageString);
-                for (WebSocketChannel peer : channel.getPeerConnections()) {
-                    WebSockets.sendText(messageString, peer, null);
+                String command = parseCommand(messageString);
+                System.out.println("Command=" + command + " Message=" + messageString);
+                switch(command) {
+                	case "HELLO": handleHelloCommand(channel, messageString);
+                		break;
+                	case "OFFER": handleOfferCommand(channel, messageString);
+            			break;
+                	case "ANSWER": handleAnswerCommand(channel, messageString);
+            			break;
+                	case "WHO": handleWhoCommand(channel);
+                		break;
+                	default:
+                		throw new InvalidParameterException("Invalid command: " + command);
                 }
             }
-            @Override
+            
+			@Override
             protected void onClose(WebSocketChannel webSocketChannel, StreamSourceFrameChannel channel) {
                 System.out.println("Socket closed");
             }
         };
     }
+	private String parseCommand(String messageString) {
+		String [] pieces = messageString.split(" ");
+		return pieces[0];
+	}
+
+    private void handleHelloCommand(WebSocketChannel channel, String messageString) {
+        sendToAllPeers(channel, messageString);
+		clients.add(messageString);
+	}
+
+    private void handleOfferCommand(WebSocketChannel channel, String messageString) {
+        sendToAllPeers(channel, messageString);
+	}
+
+    private void handleAnswerCommand(WebSocketChannel channel, String messageString) {
+        sendToAllPeers(channel, messageString);
+	}
+
+    private void handleWhoCommand(WebSocketChannel channel) {
+    	sendToAllPeers( channel, JsonStream.serialize(clients));
+	}
+    
+	private void sendToAllPeers(WebSocketChannel channel, String messageString) {
+		for (WebSocketChannel peer : channel.getPeerConnections()) {
+            WebSockets.sendText(messageString, peer, null);
+        }
+	}
 
 }
